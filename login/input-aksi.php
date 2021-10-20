e9f76afb794e026fe7a5d6b045294f62d78ea5ea
Mang Eda <54709755+wedawesnawa@users.noreply.github.com>
2021-10-20 22:09:21 +0800

    Delete login directory

--- login/input-aksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'koneksi.php';
-$nama_pemesan = $_POST['nama_pemesan'];
-$no_tlp = $_POST['no_tlp'];
-
-mysqli_query($koneksi,"INSERT INTO `order`(`nama_pemesan`, `no_tlp`) VALUES ('$nama_pemesan','$no_tlp');");
-
-//header("location:index.php?pesan=input");
-  ?>
